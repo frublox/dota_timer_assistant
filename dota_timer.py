acd4fe76c5369efe52523be0fc12c8c71032acb7
frublox <darkeneddan@gmail.com>
2015-05-31 18:36:57 -0400

    Added unit test for get_cooldown_time.

--- dota_timer.py
+++ dota_timer.py
@@ -287,5 +287,5 @@ def main():
 
 
 if __name__ == '__main__':
-    # test.run()
-    main()
+    test.run()
+    # main()
--- test.py
+++ test.py
@@ -5,4 +5,20 @@ from threading import Thread
 
 
 def run():
-    pass
+    dt.HERO_DATA = dt.read_hero_data(dt.HERO_DATA_FILE)
+
+    hero_names = ['sven', 'pudge', 'witch doctor', 'wr', 'pugna']
+
+    dt.heroes = dt.get_heroes(hero_names, [dt.get_hero_id(name) for name in hero_names])
+
+    for name in hero_names:
+        cooldowns(name)
+
+
+def cooldowns(name):
+    cooldown_time = dt.get_cooldown_time(name)
+
+    if dt.heroes[name]['has_scepter']:
+        assert cooldown_time in dt.heroes[name]['scepter_cooldowns']
+    else:
+        assert cooldown_time in dt.heroes[name]['cooldowns']
