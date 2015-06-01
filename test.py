ee3940335d507fa1547ce1d937ed806cb7627432
frublox <darkeneddan@gmail.com>
2015-06-01 19:22:40 -0400

    Removed unnecessary state assignment.

--- test.py
+++ test.py
@@ -11,8 +11,6 @@ def run():
 
     dt.heroes = dt.get_heroes(hero_names, [dt.get_hero_id(name) for name in hero_names])
 
-    dt.heroes['sven']['state'] = dt.LEVEL_6
-
     for name in hero_names:
         cooldowns(name)
 
