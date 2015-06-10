e29387e22b67d6e54f60074550e55415b1c20706
frublox <darkeneddan@gmail.com>
2015-06-10 17:55:34 -0400

    Split install_curses into two files + specified Python 2.7 path for pip.

---
+++ install_curses_py64bit.bat
@@ -0,0 +1,6 @@
+@echo off
+echo If Python is not installed in C:\Python27, run this command using wherever your pip is located.
+
+C:\Python27\pip install curses-2.2-cp27-none-win_amd64.whl
+
+PAUSE
