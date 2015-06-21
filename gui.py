044098d9efe9b65c1542b5b38ec75a01c7f9c6c3
vort3 <vort3@users.noreply.github.com>
2015-06-21 06:29:49 +0600

    Update gui.py

--- gui.py
+++ gui.py
@@ -4,22 +4,20 @@ import Tkinter as Tk
 
 import dota_timer
 
-
 class Overlay(Tk.Frame):
     def update_timer(self):
-        timer_text = ""
-
         for i, time in enumerate(dota_timer.timer_time_left):
-            timer_text += "Hero {}: {}s\n".format(i + 1, time)
 
-        self.timer['text'] = timer_text
-        self.timer.pack({'side': 'left'})
+            self.timers[i]['text'] = time
 
         dota_timer.root.after(100, self.update_timer)
 
     def __init__(self, master=None):
         Tk.Frame.__init__(self, master)
+        
+        self.timers = []
 
-        self.timer = Tk.Label()
-        self.timer['text'] = 'Timer 1: 0'
-        self.timer.pack({'side': 'left'})
+        for i in range(6):
+            self.timers.append(Tk.Label(width=8))
+            self.timers[i]['text'] = ''
+            self.timers[i].pack({'side': 'left'})
